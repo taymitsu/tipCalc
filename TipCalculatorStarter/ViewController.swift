e4aae3fd8304be2d19f833dd735511c8ca369edf
Tay <tmitsumori96@gmail.com>
2022-04-21 20:56:19 -0700

    Calculator logic added

--- TipCalculatorStarter/ViewController.swift
+++ TipCalculatorStarter/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var themeSwitch: UISwitch!
     
     @IBOutlet weak var inputCardView: UIView!
-    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var billAmountTextField: BillAmountTextField!
     @IBOutlet weak var tipPercentSegementedControl: UISegmentedControl!
     
     
@@ -29,8 +29,56 @@ class ViewController: UIViewController {
     
     // MARK: - View Lifecycle
     
+    func calculate() {
+        // dismiss keyboard
+        if self.billAmountTextField.isFirstResponder {
+            self.billAmountTextField.resignFirstResponder()
+        }
+
+        guard let billAmountText = self.billAmountTextField.text,
+            let billAmount = Double(billAmountText) else {
+            clear()
+                return
+        }
+
+        let roundedBillAmount = (100 * billAmount).rounded() / 100
+
+        let tipPercent: Double
+        switch tipPercentSegementedControl.selectedSegmentIndex {
+        case 0:
+            tipPercent = 0.15
+        case 1:
+            tipPercent = 0.18
+        case 2:
+            tipPercent = 0.20
+        default:
+            preconditionFailure("Unexpected index.")
+        }
+
+        let tipAmount = roundedBillAmount * tipPercent
+        let roundedTipAmount = (100 * tipAmount).rounded() / 100
+
+        let totalAmount = roundedBillAmount + roundedTipAmount
+
+        // Update UI
+        self.billAmountTextField.text = String(format: "%.2f", roundedBillAmount)
+        self.tipAmountLabel.text = String(format: "%.2f", roundedTipAmount)
+        self.totalAmountLabel.text = String(format: "%.2f", totalAmount)
+    }
+    
+    func clear() {
+        billAmountTextField.text = nil
+        tipPercentSegementedControl.selectedSegmentIndex = 0
+        tipAmountLabel.text = "$0.00"
+        totalAmountLabel.text = "$0.00"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        billAmountTextField.calculateButtonAction = {
+            self.calculate()
+        }
     }
     
     @IBAction func themeToggled(_ sender: UISwitch) {
@@ -42,11 +90,11 @@ class ViewController: UIViewController {
     }
     
     @IBAction func tipPercentChanged(_ sender: UISegmentedControl) {
-        
+        calculate()
     }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
-        print("reset button tapped")
+        clear()
     }
 }
 
